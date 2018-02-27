04587aafaabed61047cb320477331367a64a317b
Scott Atkins <satkins16@gmail.com>
2018-02-27 02:14:59 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ export class ButcherShop extends React.Component {
   render() {
     return React.createElement('div', { className: 'butcher-shop' }, [
       React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
-      React.createElement('ul', {},
+      React.createElement('ul', {}, 
         BUTCHER_PRODUCTS.map(product => React.createElement('li', {}, product))
       )
     ]);
